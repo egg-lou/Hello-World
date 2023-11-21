334c482468bddff114fbe0b2261a2c9f6717fe54
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2023-11-21 10:44:32 +0800

    created a palindrome program at palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    s = s.replace(" ", "").lower()
+    return s == s[::-1]
+
+user_input = input("Enter a string: ")
+if is_palindrome(user_input):
+    print(f"{user_input} is a palindrome!")
+else:
+    print(f"{user_input} is not a palindrome.")
