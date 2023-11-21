59b4f1effd156e2eeaaef69b785023c623d54a65
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2023-11-21 11:01:33 +0800

    add print statement to palindrome

--- palindrome.py
+++ palindrome.py
@@ -2,6 +2,33 @@ def is_palindrome(s):
     s = s.replace(" ", "").lower()
     return s == s[::-1]
 
+print("Welcome to the Palindrome Program!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 user_input = input("Enter a string: ")
 if is_palindrome(user_input):
     print(f"{user_input} is a palindrome!")
